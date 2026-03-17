96dfaaf9e28d1b63bb7c28568fb7a9e8b912d814
Gabi Rousine <gavriel.gav@gmail.com>
2026-03-17 22:27:58 +0200

    added more logic to the deal

--- game/deal.asm
+++ game/deal.asm
@@ -9,50 +9,53 @@ $dealer_value_addr=185
 ===============================
 ===============================
 
-$r_deal_card=R0
-$r_deal_player_or_dealer=R1
-$r_deal_len_addr=R2
-$r_deal_len=R3
-$r_deal_cards_addr=R4
-$r_deal_value_addr=R5
-$r_deal_value=R6
+$deal.r.card=R0
+$deal.r.player_or_dealer=R1
+$deal.r.len_addr=R2
+$deal.r.len=R3
+$deal.r.cards_addr=R4
+$deal.r.value_addr=R5
+$deal.r.value=R6
+$deal.r.value_current=R7
 
 // input param (player or dealer)
 // player = 0
 // dealer = 1
 
 &deal:
-POP r_deal_player_or_dealer
-DEC r_deal_player_or_dealer
-LDR r_deal_len_addr, dealer_len_addr
-LDR r_deal_cards_addr, dealer_cards_addr
-LDR r_deal_value_addr, dealer_value_addr
+POP deal.r.player_or_dealer
+DEC deal.r.player_or_dealer
+LDR deal.r.len_addr, dealer_len_addr
+LDR deal.r.cards_addr, dealer_cards_addr
+LDR deal.r.value_addr, dealer_value_addr
 JZ @dealer
-LDR r_deal_len_addr, player_len_addr
-LDR r_deal_cards_addr, player_cards_addr
-LDR r_deal_value_addr, player_value_addr
+LDR deal.r.len_addr, player_len_addr
+LDR deal.r.cards_addr, player_cards_addr
+LDR deal.r.value_addr, player_value_addr
 &dealer:
 
 CAL @get_random_card
-POP r_deal_card
+POP deal.r.card
 
-RRR r_deal_len, r_deal_len_addr
-RRR r_deal_value_addr, r_deal_value
+RRR deal.r.len, deal.r.len_addr
+RRR deal.r.value_addr, deal.r.value
 
-ADD r_deal_cards_addr, r_deal_len
-RWR r_deal_cards_addr, r_deal_card
+ADD deal.r.cards_addr, deal.r.len
+RWR deal.r.cards_addr, deal.r.card
 
-INC r_deal_len
-RWR r_deal_len_addr, r_deal_len
+INC deal.r.len
+RWR deal.r.len_addr, deal.r.len
 
-PSH r_deal_card
+PSH deal.r.card
 CAL @get_card_value
-POP r_deal_value
+POP deal.r.value
 
-RWR r_deal_value_addr, r_deal_value
+RRR deal.r.value_current, deal.r.value_addr
+ADD deal.r.value, deal.r.value_current
+RWR deal.r.value_addr, deal.r.value
 
-INC r_deal_player_or_dealer
-PSH r_deal_player_or_dealer
+PSH deal.r.card
+PSH deal.r.len
 CAL @draw_card
 
 &deal_done:
@@ -61,44 +64,69 @@ RTN
 ===============================
 ===============================
 
-$r_get_random_random_value=RNDRANGE
+$get_random_card.r.random_value=RNDRANGE
 
-$r_get_random_card_index=R0
-$r_get_random_card_value=R1
-$r_get_random_card_taken_card=R2
+$get_random_card.r.index=R0
+$get_random_card.r.value=R1
+$get_random_card.r.taken_card=R2
 
 // return card
 
 &get_random_card:
-LDR r_get_random_card_taken_card, taken
+LDR get_random_card.r.taken_card, taken
 
-LD r_get_random_card_index, r_get_random_random_value
-DEC r_get_random_card_index
+LD get_random_card.r.index, get_random_card.r.random_value
+DEC get_random_card.r.index
 
-RRR r_get_random_card_value, r_get_random_card_index
+RRR get_random_card.r.value, get_random_card.r.index
 
-AND r_get_random_card_taken_card, r_get_random_card_value
+AND get_random_card.r.taken_card, get_random_card.r.value
 JNZ @get_random_card
 
 // set to taken
-OR r_get_random_card_value, taken
+OR get_random_card.r.value, taken
 
 // update cards in ram
-RWR r_get_random_card_index, r_get_random_card_value
+RWR get_random_card.r.index, get_random_card.r.value
 
 // return value
-PSH r_get_random_card_value
+PSH get_random_card.r.value
 &get_random_card_done:
 RTN
 
 ===============================
 ===============================
 
+$get_card_value.r.card=R0
+$get_card_value.r.card_value_and=R1
+$get_card_value.r.10=R2
+
+// after 10 should return 10
+$get_card_value.v.10=10
+// 00001111
+$get_card_value.v.card_value_and=15
+
+
 // input param card
 // return int value
 
 &get_card_value:
+POP get_card_value.r.card
 
+LDR get_card_value.r.10, get_card_value.v.10
+LDR get_card_value.r.card_value_and, get_card_value.v.card_value_and
+
+// get only the number on card no suit
+AND get_card_value.r.card, get_card_value.r.card_value_and
+
+COM get_card_value.r.card, get_card_value.r.10
+JNC @more_then_10
+PSH get_card_value.r.card
+JMP @get_card_value_done
+
+&more_then_10:
+PSH get_card_value.r.10
+JMP @get_card_value_done
 
 &get_card_value_done:
 RTN
@@ -107,10 +135,127 @@ RTN
 ===============================
 ===============================
 
-// input param (player or dealer)
+$draw_card.r.player_or_dealer=R0
+$draw_card.r.len=R1
+$draw_card.r.card=R2
+$draw_card.r.value=R3
+$draw_card.r.suit=R4
+
+$draw_card.r.value_and=R5
+// 00001111
+$draw_card.v.value_and=15
+
+$sdraw_card.r.suit_and=R6
+// 00110000
+$draw_card.v.suit_and=30
+
+$draw_card.r.suit_find=R7
+
+// 00000000
+$draw_card.v.heart=0
+// 00010000
+$draw_card.v.spade=8
+// 00100000
+$draw_card.v.diamond=10
+// 00110000
+$draw_card.v.club=30
+
+// input param len
+// input param card
+
 &draw_card:
+POP draw_card.r.len
+POP draw_card.r.card
+
+LDR draw_card.r.value_and, draw_card.v.value_and
+AND draw_card.r.value_and, draw_card.r.card
+
+LD draw_card.r.value, draw_card.r.value_and
+
+LDR draw_card.r.suit_and, draw_card.v.suit_and
+AND draw_card.r.suit_and, draw_card.r.card
+
+LD draw_card.r.suit, draw_card.r.suit_and
+
+// set y axis based on player/dealer
+
+// set x axis for the suit
+
+LD draw_card.r.suit_find, draw_card.v.heart
+COM draw_card.r.suit_find, draw_card.r.suit
+JZ @draw_heart
+
+LD draw_card.r.suit_find, draw_card.v.spade
+COM draw_card.r.suit_find, draw_card.r.suit
+JZ @draw_spade
+
+LD draw_card.r.suit_find, draw_card.v.diamond
+COM draw_card.r.suit_find, draw_card.r.suit
+JZ @draw_diamond
+
+LD draw_card.r.suit_find, draw_card.v.club
+COM draw_card.r.suit_find, draw_card.r.suit
+JZ @draw_club
+
+==================================
+
+&draw_heart_symbol:
+WD p_symbol_addr, symbol_heart
+WD p_pixel_color_addr, color_red
+CAL @draw_symbol
+JMP @draw_card_symbol
+
+&draw_spade_symbol:
+WD p_symbol_addr, symbol_spade
+WD p_pixel_color_addr, color_black
+CAL @draw_symbol
+JMP @draw_card_symbol
+
+&draw_diamond_symbol:
+WD p_symbol_addr, symbol_diamond
+WD p_pixel_color_addr, color_red
+CAL @draw_symbol
+JMP @draw_card_symbol
+
+&draw_club_symbol:
+WD p_symbol_addr, symbol_club
+WD p_pixel_color_addr, color_black
+CAL @draw_symbol
+JMP @draw_card_symbol
+
+==================================
+
+&draw_card_symbol:
+// set x axis for the card
+WD p_symbol_addr, draw_card.r.value
+WD p_pixel_color_addr, color_red
+CAL @draw_symbol
 
 &draw_card_done:
-RTN
+RTN 
 
+===============================
+===============================
+
+&set_y_axis:
+// WD p_y_axis_addr, 1
+// WD p_y_axis_addr, 16
+&set_y_axis_done:
+
+===============================
+===============================
+
+&set_suit_x_axis:
+// x=1+10(n−1)
+&set_suit_x_axis_done:
+
+===============================
+===============================
+
+&set_card_x_axis:
+// x=6+10(n−1)
+&set_card_x_axis_done:
+
+===============================
+===============================
 
